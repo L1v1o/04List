5d14e45a5f6100d3b848592516457ebcc6c40c48
Livio Meuli <livio.meuli@ksb-sg.ch>
2022-06-29 20:11:28 +0200

    Video 19 min 26

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,34 +10,42 @@ import {Subject} from "rxjs";
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Apored',
-      'JAAAA MOIN LEUDE Apos auf trockn am start meine Freunde',
-      'https://www.bravo.de/assets/field/image/apored_1.jpg',
-      [
-        new Ingredient('Freshe Yeezys', 2),
-        new Ingredient('Apoburgies', 1),
-        new Ingredient('Leon Machère', 1)
-      ]),
-    new Recipe('Wurscht',
-      'Kackwurscht in meine Arsch, dicke fette Kackwurscht',
-      'https://www.op-online.de/bilder/2020/10/20/90074726/1800945-2125754472-schimmliges-brot-2uQIjHSga3ec.jpg',
-      [
-        new Ingredient('Fleisch', 1),
-        new Ingredient('Brüzelwuz', 5)
-      ]),
-    new Recipe('Joloschnitzel',
-      'Jetzt frisch im Kühlregal',
-      'https://amp.infranken.de/storage/image/6/0/9/2/2422906_noscale_1qcD8e_DYBUa8.jpg',
-      [
-        new Ingredient('Fleisch', 1),
-        new Ingredient('Chipensnuggets', 20),
-        new Ingredient('Saftbratusmaximus', 1)
-      ])
-  ];
+  // private recipes: Recipe[] = [
+    // new Recipe(
+      //  'Apored',
+      // 'JAAAA MOIN LEUDE Apos auf trockn am start meine Freunde',
+      // 'https://www.bravo.de/assets/field/image/apored_1.jpg',
+      // [
+        //  new Ingredient('Freshe Yeezys', 2),
+        //  new Ingredient('Apoburgies', 1),
+        //  new Ingredient('Leon Machère', 1)
+        //  ]),
+    //  new Recipe('Wurscht',
+      // 'Kackwurscht in meine Arsch, dicke fette Kackwurscht',
+      // 'https://www.op-online.de/bilder/2020/10/20/90074726/1800945-2125754472-schimmliges-brot-2uQIjHSga3ec.jpg',
+      //  [
+        //   new Ingredient('Fleisch', 1),
+        //   new Ingredient('Brüzelwuz', 5)
+        //  ]),
+    // new Recipe('Joloschnitzel',
+      // 'Jetzt frisch im Kühlregal',
+      // 'https://amp.infranken.de/storage/image/6/0/9/2/2422906_noscale_1qcD8e_DYBUa8.jpg',
+      // [
+        //   new Ingredient('Fleisch', 1),
+        //   new Ingredient('Chipensnuggets', 20),
+        //   new Ingredient('Saftbratusmaximus', 1)
+        //  ])
+    //];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]){
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
 
   getRecipes() {
     return this.recipes.slice();
